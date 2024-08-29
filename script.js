c824fc25edfc1f83040171a628e338aff2a4263f
todoor.17 <todorioan02@gmail.com>
2024-08-29 17:04:47 +0300

    Added two buttons

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function printMessage() {
-    document.getElementById("message").innerHTML = "Te iubesc bibi!";
-    document.getElementById("myButton").style.display = "none";
+function changeColorRandom() {
+    const randomInt = Math.floor(Math.random() * 16777216);
+    const randomColor = '#' + randomInt.toString(16).padStart(6, '0');
+    console.log(randomColor);
+    document.body.style.backgroundColor = randomColor;
+}
+
+function changeColorControlled() {
+    const colors = ["#ff80ed", "#065535", "#000000", "#ff0000", "#ffffff", "#0000ff", "#8a2be2", "#ccff00"];
+    document.body.style.backgroundColor = colors[Math.floor(Math.random() * 9)];
 }
