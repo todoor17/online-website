d697c1287d0d995a6b3a4270486c05debab3491e
todoor.17 <todorioan02@gmail.com>
2024-09-10 13:20:52 +0300

    Main and Blackjack pages added

--- blackjack/script.js
+++ blackjack/script.js
@@ -4,11 +4,15 @@ let path;
 let card;
 let playerScore = 0;
 let computerScore = 0;
-let count = 0;
-let count1 = 1;
+let count;
+let count1;
+let balance = 100;
 
 const computerText = document.getElementById("computer-score");
 const playerText = document.getElementById("player-score");
+const hitButton = document.getElementById("hitButton");
+const standButton = document.getElementById("standButton");
+const balanceText = document.getElementsByClassName("balance")[0];
 
 function generateDeck() {
     const suits = ["♠","♣","♥","♦"];
@@ -19,20 +23,31 @@ function generateDeck() {
         }
     }
 }
+
 function hit() {
     path = getRandomCard();
 
     const cardImages = document.getElementsByClassName("card");
+    console.log(count);
+    console.log((count % 2) + 2);
     cardImages[(count % 2) + 2].src = path;
 
-    console.log(deck.length)
     card = path[5];
-
     count++;
-    console.log(count);
 
-    playerScore += parseInt(getScore(card));
+
+    if (playerScore > 11 && (parseInt(getScore(card))) == 1) {
+        playerScore += 1;
+    } else {
+        playerScore += parseInt(getScore(card));
+    }
     playerText.textContent = playerScore;
+    console.log(playerScore);
+
+    if (playerScore > 21) {
+        youLost();
+        return;
+    }
 }
 
 function stand() {
@@ -42,23 +57,30 @@ function stand() {
         cardImages[count1 % 2].src = path;
 
         card = path[5];
-        computerScore += parseInt(getScore(card));
+        if (computerScore > 11 && (parseInt(getScore(card))) == 1) {
+            computerScore += 1;
+        } else {
+            computerScore += parseInt(getScore(card));
+        }
         computerText.textContent = computerScore;
         count1++;
         setTimeout(stand, 1000);
+        return;
     }
 
-    if (computerScore > playerScore) {
-        youLost();
-    } else {
+    if ((computerScore > 21 && playerScore <= 21) || computerScore < playerScore) {
         youWon();
+    } else if (computerScore == playerScore) {
+        draw();
+    } else {
+        youLost();
     }
 }
 
 function getScore(card) {
     switch (card) {
         case 'A':
-            return 11
+            return 11;
         case 'J':
         case 'K':
         case 'Q':
@@ -72,16 +94,30 @@ function getScore(card) {
 }
 
 function youLost() {
-    playerText.textContent = "YOU LOST";
+    playerText.textContent = playerScore + "-" + computerScore + ", YOU LOST";
     playerText.style.color = "red";
+    disableButtons();
 }
 
 function youWon() {
-    playerText.textContent = "YOU WON";
+    balance += 20;
+    balanceText.textContent = balance;
+    playerText.textContent = playerScore + "-" + computerScore + ", YOU WON";
     playerText.style.color = "green";
+    disableButtons();
+}
+
+function draw() {
+    balance += 20;
+    balanceText.textContent = balance;
+    playerText.textContent = playerScore + "-" + computerScore + ", YOU DRAW";
+    playerText.style.color = "black";
+    playerText.style.stroke = "1px";
+    disableButtons();
 }
 
 function reset() {
+    disableButtons();
     playerScore = 0; 
     computerScore = 0;
     count = 0;
@@ -95,20 +131,29 @@ function reset() {
 }
 
 function start() {
+    enableButtons();
     playerScore = 0;
     computerScore = 0;
-    count = 0;
+    count = 1;
     count1 = 1;
+    balance -= 10;
+    balanceText.textContent = balance;
+    
 
     path = getRandomCard();
+    const path1 = getRandomCard();
     const card = path[5];
+    const card1 = path1[5];
     computerScore = parseInt(getScore(card));
+    playerScore = parseInt(getScore(card1))
      
     computerText.textContent = computerScore;
     playerText.textContent = playerScore;
+
+    playerText.style.color = "black";
     document.getElementsByClassName("card")[0].src = path;
     document.getElementsByClassName("card")[1].src = "images/black.png";
-    document.getElementsByClassName("card")[2].src = "images/white.png";
+    document.getElementsByClassName("card")[2].src = path1;
     document.getElementsByClassName("card")[3].src = "images/white.png";
     console.log(computerScore);
 
@@ -121,4 +166,14 @@ function getRandomCard() {
     let random = Math.floor(Math.random() * deck.length);
     let randomPath = path = "deck/" + deck[random] + ".png";
     return randomPath;
+}
+
+function disableButtons() {
+    hitButton.disabled = true;
+    standButton.disabled = true;
+}
+
+function enableButtons() {
+    hitButton.disabled = false;
+    standButton.disabled = false;
 }
