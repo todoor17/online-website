cbae34a1a4e03a0418ebeafda7b6c7fe1a14e122
todoor.17 <todorioan02@gmail.com>
2024-09-13 17:35:08 +0300

    --allow-empty

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 function toggleMenu(x) {
     x.classList.toggle("change");
     document.querySelector(".sidebar").classList.toggle("show");
+    document.querySelector(".content-container").classList.toggle("shift")
 }
+
+function setDate() {
+    let date = new Date();
+    let day = date.getDay();
+    let month = date.getMonth();
+    const year = date.getFullYear();
+
+    if (parseInt(day) < 10) {
+        day = "0" + day;
+    }
+
+    if (parseInt(month) < 10) {
+        month = "0" + month;
+    }
+
+    date = day + "." + month + "." + year;
+    document.getElementsByClassName("date")[0].textContent = date;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setDate();
+})
