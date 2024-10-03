52ff7de1a5c0c8e31fe3253761e7a47e8d59ea85
todoor17 <todorioan02@gmail.com>
2024-10-03 09:48:09 +0300

    Image slider added

--- help/script.js
+++ help/script.js
@@ -2,30 +2,78 @@ const btnLeft = document.getElementById("btnLeft");
 const btnRIght = document.getElementById("btnRight");
 
 const image = document.getElementsByClassName("image")[0];
+const link = document.getElementById("link");
+const text = document.getElementsByClassName("text")[0];
 
-const bjPath = "images/blackjack.png";
-const calculatorPath = "images/calculator.png";
-const ticTacToePath = "images/tic-tac-toe.png";
-const stopwatch = "images/stopwatch.png";
+const bjImgPath = "images/blackjack.png";
+const calculatorImgPath = "images/calculator.png";
+const ticTacToeImgPath = "images/tic-tac-toe.png";
+const stopwatchImgPath = "images/stopwatch.png";
+const bjPath = "../blackjack/index.html";
+const calculatorPath = "../calculator/index.html";
+const ticTacToePath = "../tic-tac-toe/index.html";
+const stopwatchPath = "../stopwatch/index.html";
 
-const images = [bjPath, calculatorPath, ticTacToePath, stopwatch];
+const images = [bjImgPath, calculatorImgPath, ticTacToeImgPath, stopwatchImgPath];
+const paths = [bjPath, calculatorPath, ticTacToePath, stopwatchPath];
+
+let firstTouchX = 0, lastTouchX = 0;
 
 let count = 0;
 
-btnLeft.addEventListener("click", function() {
-    if (count == 0) {
-        count = 3;
-    } else {
-        count--;
-    }
-    image.src = images[count % 4];
-});
+// btnLeft.addEventListener("click", function() {
+//     if (count == 0) {
+//         count = 3;
+//     } else {
+//         count--;
+//     }
+//     image.src = images[count % 4];
+//     link.href = paths[count % 4];
+// });
 
-btnRight.addEventListener("click", function() {
-    count++;
-    image.src = images[count % 4];
-});
+// btnRight.addEventListener("click", function() {
+//     count++;
+//     image.src = images[count % 4];
+//     link.href = paths[count % 4];
+// });
 
 document.addEventListener("DOMContentLoaded", function() {
     image.src = "images/blackjack.png";
+    //link.href = paths[count % 4];
+});
+
+image.addEventListener("touchstart", function(event) {
+    const touch = event.changedTouches[0];
+    console.log("First touchX: " + touch.screenX);
+    firstTouchX = touch.screenX;
+});
+
+image.addEventListener("touchend", function(event) {
+    const touch = event.changedTouches[0];
+    console.log("Last touchX: " + touch.screenX);
+    lastTouchX = touch.screenX;
+    console.log ("First touch: " + firstTouchX + " Last touch: " + lastTouchX);
+
+    if (firstTouchX > lastTouchX) { // swipe left
+        if (count == 0) {
+            count = 3;
+        } else {
+            count--;
+        }
+        image.src = images[count % 4];
+        link.href = paths[count % 4];
+    } else if (firstTouchX == lastTouchX) {
+        
+    }
+    else { // swipe right
+        count++;
+        image.src = images[count % 4];
+        link.href = paths[count % 4];
+    } 
+});
+
+btnLeft.addEventListener("click", function() {
+    image.style.opacity = 0;
+    text.style.opacity = 1;
+    console.log("here");
 });
